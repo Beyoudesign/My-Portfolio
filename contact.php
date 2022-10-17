55271113375acc18dd5e150f9cb1460ddb655d92
beyoudesign <cjohns48@lesley.edu>
2022-10-16 20:16:37 -0400

    added form to contact page

---
+++ contact.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html>
+
+<?php
+    include 'header.php';
+    include 'navigation.php';
+    include 'logo.php';
+
+?>
+   
+
+   
+    
+    
+    
+    
+    <body>
+    <form action="submit-post.php" method="POST">
+            <input type="text" name="name" placeholder="Name">
+            <br><br>
+            <input type="text" name="date" placeholder="Date">
+            <br><br>
+            <input type="date" name="email" placeholder="Email Address">
+            <br><br>
+            <input type="text" name="work" placeholder="What can I do for you?">
+            <br><br>
+            <button type="submit" name="Submit">Contact me</button>
+            
+            
+
+    </form>
+        
+
+    </body>
+</html>
--- navigation.php
+++ navigation.php
@@ -2,7 +2,7 @@
         <div id="navigation">
             <a href="index.php">who I am</a>
             <a href="work.php">my work</a>
-            <a href="resume.php">resume</a>
+            <a href="contact.php">contact</a>
 
         </div>
 
